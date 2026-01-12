730ec96687229f62e08035a150da58b5675a1b50
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2026-01-12 11:20:17 +0100

    testing

--- src/models/Cart.ts
+++ src/models/Cart.ts
@@ -1,5 +1,16 @@
+// import type { CartItem } from "./CartItem";
 import type { Product } from "./product";
 
 export type Cart = {
-  items: Product[];
+  items: CartItem[] | [];
+};
+
+export type CartItem = {
+  product: Product;
+  amount: number;
+};
+
+export type SubTotal = {
+  itemTotal: number;
+  freightTotal: number;
 };
