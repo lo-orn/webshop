bddea76fe01b16d6e591eb990497b8b835c188cb
Emma Riklund <emmariklund@hotmail.com>
2026-01-16 11:50:23 +0100

    update functions

--- src/pages/cart/cartPage.ts
+++ src/pages/cart/cartPage.ts
@@ -1,4 +1,13 @@
-import type { Product } from "../../models/product";
+import type { Cart } from "../../models/Cart";
+import type { CartItem } from "../../models/CartItem";
+import { addItemToCart, findCart, removeOneItemFromCart } from "../../utils/cartUtils";
+import "..//../scss/cart.scss"
+
+const getCartFromLS = (): Cart | null => {
+    const cartString = localStorage.getItem("cart");
+    if (!cartString) return null;
+    return JSON.parse(cartString);
+  };
 
 // The base (section + h1)
 function createCartSection(): HTMLElement {
@@ -38,74 +47,161 @@ function createEmptyCartView(): HTMLElement {
 }
 
 // Filled cart view
-function createFilledCartView(product: Product): HTMLElement {
+function createCartItem(item: CartItem, onChange: () => void): HTMLElement {
     const cartContainer = document.createElement("div");
     cartContainer.className = "cart-container";
-
+ 
     const img = document.createElement("img");
     img.className = "cart-container__img";
-    img.src = product.image;
-    img.alt = product.name;
-
+    img.src = item.product.image;
+    img.alt = item.product.name;
+ 
     const productName = document.createElement("p");
     productName.className = "cart-container__p";
-    productName.textContent = product.name;
-
+    productName.textContent = item.product.name;
+ 
     const productPrice = document.createElement("p");
     productPrice.className = "cart-container__price";
-    productPrice.textContent = `${product.price}SEK`; 
-
+    productPrice.textContent = `${item.product.price} SEK`;
+ 
+    const qtyWrap = document.createElement("div");
+    qtyWrap.className = "cart-qty"
+ 
+    const qtyText = document.createElement("span");
+    qtyText.className = "qty";
+    qtyText.textContent = String(item.amount);
+ 
     const buttonMinus = document.createElement("button");
     buttonMinus.className = "qty-minus";
-    buttonMinus.addEventListener = 
-
+ 
     const buttonPlus = document.createElement("button");
     buttonPlus.className = "qty-plus";
-    buttonPlus.addEventListener = 
-
+ 
     const buttonRemove = document.createElement("button");
     buttonRemove.className = "removeItems";
-
-    // Order summery section
-    const wrapperSummery = document.createElement("div");
-    wrapperSummery.className = "div-summery";
-
-    const headingSummery = document.createElement("p");
-    headingSummery.className = "div-summery__heading"
-    headingSummery.textContent = "ORDER SUMMARY"
-
-    const promoText = document.createElement("p");
-    promoText.className = "promoText";
-    promoText.textContent = "PROMO CODE";
-
-    const form = document.createElement("form");
-    form.className = "promoForm";
-
-    const input = document.createElement("input");
-    input.className = "promoInput";
-    input.textContent = "ENTER CODE";
-
-    const submitButton = document.createElement("button");
-    submitButton.className = "btn-submit";
-    submitButton.textContent = "SUBMIT";
-
-    const subTotal = document.createElement("p");
-    subTotal.className = "subTotal-text";
-    subTotal.textContent = "SUBTOTAL";
-
-    const shipping = document.createElement("p");
-    shipping.className = "shipping-text";
-    shipping.textContent = "SHIPPING";
-
-    const total = document.createElement("p");
-    total.className = "total-text";
-    total.textContent = "TOTAL";
-
-    const continueBtn = document.createElement("button");
-    continueBtn.className = "btn-continue";
-    continueBtn.textContent = "CONTINUE SHOPPING";
-
-    const checkoutBtn = document.createElement("button");
-    checkoutBtn.className = "btn-checkout";
-    checkoutBtn.textContent = "CHECKOUT";  
+ 
+    
+ 
+    // Connect to cartUtils
+    buttonPlus.addEventListener("click", async () => {
+    await addItemToCart(String(item.product.id));
+    console.log("Item added");
+    onChange();
+    });
+ 
+    buttonMinus.addEventListener("click", () => {
+    removeOneItemFromCart(String(item.product.id));
+    console.log("Item removed");
+    onChange();
+    });
+ 
+    return cartContainer
 }
+
+// initCartPage function - render function + item forEach loop
+export const initCartPage = () => {
+    console.log("initCartPage körs");
+ 
+    findCart();
+ 
+    const main = document.getElementById("main");
+    if(!main) return;
+ 
+    const render = () => {
+        console.log("render() körs");
+        const cart = getCartFromLS();
+        console.log("Cart now:", cart)
+        if(!cart) return;
+ 
+        main.innerHTML = "";
+ 
+        const section = createCartSection();
+        main.appendChild(section)
+ 
+        if(cart.items.length === 0) {
+            section.appendChild(createEmptyCartView());
+            return;
+        }
+ 
+        cart.items.forEach((item) => {
+        section.appendChild(createCartItem(item, render))
+    });
+ 
+    function createOrderSummery(cart: Cart): HTMLElement {
+        const wrapperSummery = document.createElement("div");
+        wrapperSummery.className = "div-summery";
+ 
+        const headingSummery = document.createElement("p");
+        headingSummery.className = "div-summery__heading"
+        headingSummery.textContent = "ORDER SUMMARY"
+
+        let subtotal = 0;
+        cart.items.forEach((item) => {
+        subtotal += item.product.price * item.amount;
+        });
+ 
+ 
+        const promoText = document.createElement("p");
+        promoText.className = "promoText";
+        promoText.textContent = "PROMO CODE";
+ 
+        const form = document.createElement("form");
+        form.className = "promoForm";
+ 
+        const input = document.createElement("input");
+        input.className = "promoInput";
+        input.textContent = "ENTER CODE";
+ 
+        const submitButton = document.createElement("button");
+        submitButton.className = "btn-submit";
+        submitButton.textContent = "SUBMIT";
+
+        const subTotalWrapper = document.createElement("div");
+        subTotalWrapper.className = "subTotalWrapper";
+ 
+        const subTotal = document.createElement("p");
+        subTotal.className = "subTotal-text";
+        subTotal.textContent = "SUBTOTAL";
+
+        const subTotalPrice = document.createElement("p");
+        subTotalPrice.className = "subTotalPrice";
+        subTotalPrice.textContent = `${cart.items}`
+
+        const shippingWrapper = document.createElement("div");
+        shippingWrapper.className = "shippingWrapper";
+ 
+        const shipping = document.createElement("p");
+        shipping.className = "shipping-text";
+        shipping.textContent = "SHIPPING";
+
+        const shippingPrice = document.createElement("p");
+        shippingPrice.className = "shippingPrice";
+        shippingPrice.textContent
+
+        const totalWrapper = document.createElement("div");
+        totalWrapper.className = "totalWrapper";
+ 
+        const total = document.createElement("p");
+        total.className = "total-text";
+        total.textContent = "TOTAL";
+
+        const totalPrice = document.createElement("p");
+        totalPrice.className = "totalPrice";
+ 
+        const continueBtn = document.createElement("button");
+        continueBtn.className = "btn-continue";
+        continueBtn.textContent = "CONTINUE SHOPPING";
+ 
+        const checkoutBtn = document.createElement("button");
+        checkoutBtn.className = "btn-checkout";
+        checkoutBtn.textContent = "CHECKOUT";  
+ 
+    return wrapperSummery
+     }
+       
+    }
+    render();
+  
+};
+ 
+initCartPage();
