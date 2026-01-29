b5f12b8581ba2e0131d661bdf81231c5eed11fa5
Lo <lovisaornestal@gmail.com>
2026-01-29 10:50:14 +0100

    changed back to org name

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         pdp: resolve(__dirname, "pdp.html"),
         checkout: resolve(__dirname, "checkout.html"),
-        cartPage: resolve(__dirname, "cartpage.html"),
+        cartPage: resolve(__dirname, "cartPage.html"),
         newDrops: resolve(__dirname, "newdrops.html"),
       },
     },
